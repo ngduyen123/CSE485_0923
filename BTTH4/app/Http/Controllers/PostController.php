fca9dd58b257b1e6de43300ba86aeac13b76606c
ngduyen123 <113083866+ngduyen123@users.noreply.github.com>
2023-10-13 21:32:59 +0700

    Add files via upload

---
+++ BTTH4/app/Http/Controllers/PostController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function getAllPosts(){
+        echo"All posts from here";
+    }
+}
